54c8e39eddc1abbd0e1c37f7bdfa167832930c19
Badyss <badyss572@gmail.com>
2023-05-19 23:18:10 +0200

    oe

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-const images= ["C++.png", "html.png", "python.png","sql.png","linux.png","java.png",];
+const images= ["C++.png", "html.png", "python.png","json.png","linux.png","js2.png",];
 let imageIndex = 0;
 
 
@@ -8,7 +8,7 @@ for (let i = 2; i <= 6; i++) {
     document.getElementById("desc" + i).style.display = 'none';
 }
 
-function changerImage() {
+function changerlogo() {
 
     const currentImage = document.getElementById("image" + (imageIndex + 1));
     const currentText = document.getElementById("texte" + (imageIndex + 1));
@@ -26,23 +26,6 @@ function changerImage() {
     nextText.style.display = 'block';
     nextDesc.style.display = 'block';
     nextImage.src = images[imageIndex];
-}
-
-const image= ["projet11.png", "projet12.png", "projet13.png", "projet14.png"]
-let proj1Index = 0;
-
-for (let i = 2; i <= 4; i++) {
-    document.getElementById("proj1" + i).style.display = 'none';
-}
-
-function changerImage2() {
-
-    const currentproj1 = document.getElementById("proj1" + (proj1Index + 1));
-    currentproj1.style.display = 'none';
-    proj1Index = (proj1Index + 1) % image.length;
-    const nextproj1 = document.getElementById("proj1" + (proj1Index + 1));
-    nextproj1.style.display = 'block';
-    nextproj1.src = image[proj1Index];
 }
 
 function intro() {
@@ -66,9 +49,25 @@ function siu() {
     });
 }
 
-function projet() {
-    document.getElementById("imagent").onclick = function() {
-        document.getElementById("affichage").style.display = "block";
+function fermermenu(affichageId, boutId) {
+        const affichage = document.getElementById(affichageId);
+        affichage.style.animation = "fadeOut 0.5s";
+
+        const elementsToUnblur = document.querySelectorAll(".blur-effect");
+        elementsToUnblur.forEach(element => {
+            element.classList.remove("blur-effect");
+        });
+        setTimeout(function() {
+            affichage.style.display = "none";
+            affichage.style.animation = "";
+        }, 500);
+    };
+
+function projet(index) {
+    document.getElementById("img" + index).onclick = function() {
+        const affichage = document.getElementById("affichage" + index);
+        affichage.style.animation = "fadeIn 0.5s";
+        affichage.style.display = "block";
         const projDiv = this.closest(".proj");
         const elementsToBlur = projDiv.querySelectorAll(":scope > *:not(.panel)");
         elementsToBlur.forEach(element => {
@@ -77,13 +76,52 @@ function projet() {
     };
 }
 
-function fermermenu() {
-    document.getElementById("bout").onclick = function() {
-        document.getElementById("affichage").style.display = "none";
-        const elementsToUnblur = document.querySelectorAll(".blur-effect");
-        elementsToUnblur.forEach(element => {
-            element.classList.remove("blur-effect");
-        });
-    };
-}
+const image = [
+    ["projet11.png", "projet12.png", "projet13.png", "projet14.png"],
+    ["projet21.png", "projet22.png"],
+    ["projet31.png", "projet32.png", "projet33.png", "projet34.png"],
+    ["projet41.png", "projet42.png"],
+    ["projet51.png", "projet52.png", "projet53.png", "projet54.png", "projet55.png", "projet56.png"]
+  ];
+  
+  const indexes = [0, 0, 0, 0, 0];
+  
+  function changerImage(index) {
+    const currentProj = document.getElementById(`proj${index + 1}${indexes[index] + 1}`);
+    currentProj.style.display = 'none';
+    currentProj.classList.add('hidden');
+    indexes[index] = (indexes[index] + 1) % image[index].length;
+    const nextProj = document.getElementById(`proj${index + 1}${indexes[index] + 1}`);
+    nextProj.style.display = 'block';
+    nextProj.classList.remove('hidden');
+    nextProj.src = image[index][indexes[index]];
+  }
+  
+  for (let i = 0; i < image.length; i++) {
+    for (let j = 1; j < image[i].length; j++) {
+      const proj = document.getElementById(`proj${i + 1}${j + 1}`);
+      proj.classList.add('hidden');
+    }
+  }
+  
 
+function handleTextClick(textId, affichageId) {
+    document.getElementById(textId).onclick = function() {
+      const affichage = document.getElementById(affichageId);
+      affichage.style.animation = "fadeIn 1s";
+      affichage.style.display = "block";
+      const projDiv = this.closest(".proj");
+      const elementsToBlur = projDiv.querySelectorAll(":scope > *:not(.panel)");
+      elementsToBlur.forEach(element => {
+        element.classList.add("blur-effect");
+      });
+    };
+  }
+  
+  handleTextClick("texte1", "affichage3");
+  handleTextClick("texte2", "affichage4");
+  handleTextClick("texte3", "affichage1");
+  handleTextClick("texte4", "affichage1");
+  handleTextClick("texte5", "affichage5");
+  handleTextClick("texte6", "affichage4");
+  
